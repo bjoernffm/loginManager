b4d6d5c2754ec85be3b75a7e296e9cc2a1732a19
Bjoern Ebbrecht <bjoern.ebbrecht@gmail.com>
2014-01-01 22:46:07 +0100

    added search functionality to php scripts

--- ajax/getLoginList.ajax.php
+++ ajax/getLoginList.ajax.php
@@ -2,7 +2,11 @@
 
 	require_once '../common.inc.php';
 	
-	$loginList = $manager->getLoginList();
+	if (isset($_GET['search']) and !empty(trim($_GET['search']))) {
+		$loginList = $manager->getLoginListByKeywords($_GET['search']);
+	} else {
+		$loginList = $manager->getLoginList();
+	}
 	
 	echo json_encode($loginList);
 
--- includes/Manager.class.php
+++ includes/Manager.class.php
@@ -51,6 +51,61 @@
 			
 			return $logins;
 		}	
+		
+		public function getLoginListByKeywords($keywords) {
+			
+			if (empty(trim($keywords)))
+				return array();
+			
+			$mysqli = self::getMysqlConnection();
+			
+			$keywords = explode(' ', $keywords);
+			$prepared = array();
+			foreach ($keywords as $keyword) {
+				$keyword = $mysqli->real_escape_string($keyword);
+				$prepared[] = '`login_user` LIKE "%' . $keyword . '%"';
+				$prepared[] = '`login_location` LIKE "%' . $keyword . '%"';
+				$prepared[] = '`login_description` LIKE "%' . $keyword . '%"';
+				$prepared[] = '`login_tags` LIKE "%' . $keyword . '%"';
+			}
+			
+			$result = $mysqli->query('SELECT
+											`logins`.*,
+											`user_logins`.`type`
+										FROM
+											`user_logins`
+										LEFT JOIN
+											`logins`
+										ON
+											`logins`.`login_id` = `user_logins`.`login_id`
+										WHERE
+											`user_id` = ' . $this->userId . ' AND
+											(
+												' . implode(' OR ', $prepared) . '	
+											)');
+											
+			$logins = array();
+			while ($row = $result->fetch_assoc()) {
+				$object = new stdClass();
+				$object->id = $row['login_id'];
+				$object->user = $row['login_user'];
+				$object->password = $row['login_password'];
+				$object->location = $row['login_location'];
+				$object->description = $row['login_description'];
+				
+				if (empty($row['login_tags'])) {
+					$object->tags = array();
+				} else {
+					$object->tags = explode(',', $row['login_tags']);
+				}
+				
+				$object->type = $row['type'];
+				
+				$logins[] = $object;
+			}
+			
+			return $logins;
+		}	
 	}
 
 ?>
