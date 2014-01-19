3b4d2878394a5c9cce28e50a3edc5f081ec39ae8
Bjoern Ebbrecht <bjoern.ebbrecht@gmail.com>
2014-01-19 19:42:37 +0100

    changed path

--- ajax/loginSession.ajax.php
+++ ajax/loginSession.ajax.php
@@ -2,8 +2,8 @@
 	use LoginManager\Manager;
 	use LoginManager\Session;
 
-	require_once 'includes/Manager.class.php';
-	require_once 'includes/Session.class.php';
+	require_once '../includes/Manager.class.php';
+	require_once '../includes/Session.class.php';
 	
 	$session = new Session();
 	$session->start();
