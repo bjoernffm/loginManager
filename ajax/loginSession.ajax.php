3df6568935b46ac06f8cf828a13a0512e4d3b093
Bjoern Ebbrecht <bjoern.ebbrecht@gmail.com>
2014-01-22 13:06:13 +0100

    added cookie path

--- ajax/loginSession.ajax.php
+++ ajax/loginSession.ajax.php
@@ -1,4 +1,5 @@
 <?
+
 	use LoginManager\Manager;
 	use LoginManager\Session;
 
@@ -12,7 +13,7 @@
 		$user = Manager::checkCredentials($_REQUEST['username'], $_REQUEST['password']);
 		
 		$session->setVar('loggedIn', true);
-		$session->setVar('userId', $user['user_id']);
+		$session->setVar('userId', (int) $user['user_id']);
 		
 		echo json_encode(array(
 			'status' => 200
--- common.inc.php
+++ common.inc.php
@@ -2,13 +2,13 @@
 
 	use LoginManager\Session;
 
-	require_once 'includes/Manager.class.php';
 	require_once 'includes/Session.class.php';
+	require_once 'includes/Manager.class.php';
 	
 	$session = new Session();
 	$session->start();
-	
-	if ($session->getVar('loggedIn') != true and NO_STOP != true)
+
+	if ($session->getVar('loggedIn') !== true)
 		exit();
 
 ?>
