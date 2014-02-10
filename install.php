e656f2b9c63e81c20f0a2c6f06c9a7248254bdd0
Bjoern Ebbrecht <bjoern.ebbrecht@gmail.com>
2014-02-10 11:10:54 +0100

    added checking functionality for mysqli

--- install.php
+++ install.php
@@ -4,6 +4,7 @@
 
 	$modules = array();
 	$error = array();
+	$errorsMysqli = array();
 	
 	/**
 	 * Check php version
@@ -69,7 +70,28 @@
 		$error[] = 'Password has/verfy functions not found - see <a href="http://de2.php.net/manual/de/ref.password.php" target="_blank">php.net</a>';
 	}
 
-	$moduleError = (count($error)>0);
+	$installAvaialable = (count($error)==0);
+	
+	if (isset($_POST['type'])) {
+		if ($_POST['type'] == 'Test settings') {
+			@$mysqli = new mysqli(
+				$_POST['inputHost'],
+				$_POST['inputUsername'],
+				$_POST['inputPassword'],
+				$_POST['inputDatabase']);
+
+			/* check connection */
+			if ($mysqli->connect_errno)
+				$errorsMysqli[] = $mysqli->connect_error;
+			
+			/* check if server is alive */
+			if (@$mysqli->ping() === false)
+				$errorsMysqli[] = $mysqli->error;
+			
+			/* close connection */
+			@$mysqli->close();
+		}
+	}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -85,58 +107,64 @@
 	</head>
 	<body>
 		<div class="container">
-			<h1>Installation</h1>
-			<hr />
-			<div class="row">
-				<div class="col-sm-6">
-					<h2>MySQL Credentials</h2>
-					<form role="form">
+			<form role="form" action="install.php" method="post">
+				<h1>Installation</h1>
+				<hr />
+				<div class="row">
+					<div class="col-sm-6">
+						<h2>MySQL Credentials</h2>
+						<?if(count($errorsMysqli)>0){?>
+						<p class="alert alert-danger">
+							<b>Error</b><br />
+							- <?=implode('<br /> - ', $errorsMysqli)?>	
+						</p>
+						<?}?>
 						<div class="form-group">
 					    	<label for="inputUsername">Username</label>
-			    			<input type="text" class="form-control" name="inputUsername" placeholder="Enter MySQL username" required />
+			    			<input type="text" class="form-control" name="inputUsername" value="<?=((isset($_POST['inputUsername']))?$_POST['inputUsername']:'')?>" placeholder="Enter MySQL username" required />
 						</div>
 						<div class="form-group">
 			    			<label for="inputPassword">Password</label>
-			    			<input type="password" class="form-control" name="inputPassword" placeholder="Enter MySQL password" required />
+			    			<input type="password" class="form-control" name="inputPassword" value="<?=((isset($_POST['inputPassword']))?$_POST['inputPassword']:'')?>" placeholder="Enter MySQL password" required />
 						</div>
 						<div class="form-group">
 			    			<label for="inputHost">Host</label>
-			    			<input type="text" class="form-control" name="inputHost" placeholder="Enter MySQL host" value="localhost" required />
+			    			<input type="text" class="form-control" name="inputHost" value="<?=((isset($_POST['inputHost']))?$_POST['inputHost']:'localhost')?>" placeholder="Enter MySQL host (normally localhost)" required />
 						</div>
 						<div class="form-group">
 			    			<label for="inputDatabase">Database</label>
-			    			<input type="text" class="form-control" id="inputDatabase" placeholder="Enter MySQL database" required />
+			    			<input type="text" class="form-control" name="inputDatabase" value="<?=((isset($_POST['inputDatabase']))?$_POST['inputDatabase']:'')?>" placeholder="Enter MySQL database" required />
 						</div>
-					</form>
+					</div>
+					<div class="col-sm-6">
+						<h2>PHP-Modules</h2>
+						<?if(count($error)>0){?>
+						<p class="alert alert-danger">
+							<b>Error</b><br />
+							- <?=implode('<br /> - ', $error)?>	
+						</p>
+						<?}?>
+						<p class="<?=$modules['php_version']['class']?>">
+							<span class="glyphicon glyphicon-<?=$modules['php_version']['icon']?>"></span> PHP 5.3
+						</p>
+						<p class="<?=$modules['mysqli_ext']['class']?>">
+							<span class="glyphicon glyphicon-<?=$modules['mysqli_ext']['icon']?>"></span> PHP MySQLi
+						</p>
+						<p class="<?=$modules['mcrypt_ext']['class']?>">
+							<span class="glyphicon glyphicon-<?=$modules['mcrypt_ext']['icon']?>"></span> PHP Mcrypt
+						</p>
+						<p class="<?=$modules['password_ext']['class']?>">
+							<span class="glyphicon glyphicon-<?=$modules['password_ext']['icon']?>"></span> PHP Password hash/verify
+						</p>
+					</div>
 				</div>
-				<div class="col-sm-6">
-					<h2>PHP-Modules</h2>
-					<?if(count($error)>0){?>
-					<p class="alert alert-danger">
-						<b>Error</b><br />
-						- <?=implode('<br /> - ', $error)?>	
-					</p>
-					<?}?>
-					<p class="<?=$modules['php_version']['class']?>">
-						<span class="glyphicon glyphicon-<?=$modules['php_version']['icon']?>"></span> PHP 5.3
-					</p>
-					<p class="<?=$modules['mysqli_ext']['class']?>">
-						<span class="glyphicon glyphicon-<?=$modules['mysqli_ext']['icon']?>"></span> PHP MySQLi
-					</p>
-					<p class="<?=$modules['mcrypt_ext']['class']?>">
-						<span class="glyphicon glyphicon-<?=$modules['mcrypt_ext']['icon']?>"></span> PHP Mcrypt
-					</p>
-					<p class="<?=$modules['password_ext']['class']?>">
-						<span class="glyphicon glyphicon-<?=$modules['password_ext']['icon']?>"></span> PHP Password hash/verify
-					</p>
+				<hr />
+				<div class="row">
+					<div class="col-sm-12">
+						<button id="btn-install" class="btn btn-block btn-success"<? if(!$installAvaialable) echo 'disabled';?>>Install LoginManager v<?=VERSION?></button>
+					</div>
 				</div>
-			</div>
-			<hr />
-			<div class="row">
-				<div class="col-sm-12">
-					<button class="btn btn-block btn-success"<? if(count($moduleError)>0) echo 'disabled';?>>Install LoginManager v<?=VERSION?></button>
-				</div>
-			</div>
+			</form>
 		</div>
 	</body>
 </html>
